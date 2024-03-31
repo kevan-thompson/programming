16a9fdd284baee2affa5973d361fe71d4eb7f83b
Kevan <kevcom2001@gmail.com>
2024-03-30 22:26:46 -0700

    Added a very simple c++ file for test if the compiler on my vm is working.

---
+++ coursera/object-oriented data structures in C++/week1/cpp_intro/intro.cpp
@@ -0,0 +1,21 @@
+/**********************************************************
+Filename:   intro.cpp
+Author:     Kevan Thompson
+Date:       March 30, 2024
+Description:A very simple program. Mostly used to make 
+            sure the compiler and other programs are 
+            installed correctly  
+**********************************************************/
+
+#include <iostream>
+
+int main(){
+
+    int i = 4;
+    char c = 'a';
+	
+    i = i + 2;
+    
+    std::cout << i << " " << c << std::endl;
+    
+}
